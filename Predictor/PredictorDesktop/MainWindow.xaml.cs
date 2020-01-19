74aafe817cc46cc8ad5de3988d930afa727ef17a
Marco Minerva <marco.minerva@gmail.com>
2020-01-19 12:24:21 +0100

    Add WPF sample

---
+++ Predictor/PredictorDesktop/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Predictor.Models;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+
+namespace PredictorDesktop
+{
+    public partial class MainWindow : Window
+    {
+        private readonly HttpClient httpClient;
+        private const string serviceUrl = "http://localhost:7071/api/";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            httpClient = new HttpClient { BaseAddress = new Uri(serviceUrl) };
+        }
+
+        private async void btnAnalyze_Click(object sender, RoutedEventArgs e)
+        {
+            var jsonContent = JsonSerializer.Serialize(new Request { Text = txtSentence.Text });
+            var requestContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("AnalyzeSentiment", requestContent);
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var prediction = JsonSerializer.Deserialize<Response>(responseContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            lblAnalyzeResult.Text = $"Sentiment: {prediction.Sentiment} (Positive Probability: {prediction.PositiveProbability:P2})";
+        }
+    }
+}
---
+++ Predictor/PredictorDesktop/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿namespace Predictor.Models
+{
+    public class Response
+    {
+        public string Text { get; set; }
+
+        public string Sentiment { get; set; }
+
+        public bool IsPositive { get; set; }
+
+        public float PositiveProbability { get; set; }
+    }
+}
